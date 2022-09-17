2166e5d46e521f61c0edbf7ca3e2f84fa49cd5b9
i1fxx <anton.burskii@gmail.com>
2022-09-17 19:26:56 +0300

    1

---
+++ precache-manifest.b15b2c6ea875fce8e443813158a969fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b048ed94c6b1a130046",
+    "url": "/static/css/main.05695ebc.chunk.css"
+  },
+  {
+    "revision": "2b048ed94c6b1a130046",
+    "url": "/static/js/main.2b048ed9.chunk.js"
+  },
+  {
+    "revision": "48e1e8d48bd601932a35",
+    "url": "/static/js/1.48e1e8d4.chunk.js"
+  },
+  {
+    "revision": "60d2dbb33df818d21333",
+    "url": "/static/css/2.d51a373e.chunk.css"
+  },
+  {
+    "revision": "60d2dbb33df818d21333",
+    "url": "/static/js/2.60d2dbb3.chunk.js"
+  },
+  {
+    "revision": "24eddbf9ec0a04bda7e7",
+    "url": "/static/js/runtime~main.24eddbf9.js"
+  },
+  {
+    "revision": "fd0e349e561df7c639fdc8f3539e2b13",
+    "url": "/index.html"
+  }
+];
